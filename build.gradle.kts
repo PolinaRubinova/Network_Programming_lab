4b1726ead10184f2fbb8c85d1a18a1c6ffbda204
Rubinova Polina <polina.rubinova@spbpu.com>
2023-04-08 16:39:30 +0300

    #7 Add Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,20 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
+tasks.create("MyFatJar", Jar::class) {
+    group = "build"
+    description = "Creates a self-contained fat JAR of the application that can be run."
+    manifest.attributes["Main-Class"] = "com.example.ApplicationKt"
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA", "META-INF/INDEX.LIST")
+    from(dependencies)
+    with(tasks.jar.get())
+}
+
 tasks.test {
     useJUnitPlatform()
 }
